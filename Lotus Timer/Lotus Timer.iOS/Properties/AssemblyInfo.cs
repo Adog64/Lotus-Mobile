44ce277da6ddadeffc313cdbe7ff2cd1135926f2
Adog64 <theelectriczlime@gmail.com>
2022-01-11 10:08:49 -0500

    Formatting adjustments for large devices

--- Lotus Timer/Lotus Timer.iOS/Properties/AssemblyInfo.cs
+++ Lotus Timer/Lotus Timer.iOS/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lotus_Timer.iOS")]
+[assembly: AssemblyTitle("LotusTimer.iOS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lotus_Timer.iOS")]
+[assembly: AssemblyCompany("TeamLotus")]
+[assembly: AssemblyProduct("LotusTimer.iOS")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
